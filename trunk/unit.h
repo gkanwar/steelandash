4de15c848781ceaf5c658db439cd801a93b36e7e
GurtejKanwar <GurtejKanwar@76a915bb-40ac-062f-18d0-0e01efab0db8>
2010-11-26 01:48:18 +0000

    A few changes until I ran into a circular dependency and decided to restart G

--- trunk/unit.h
+++ trunk/unit.h
@@ -14,14 +14,38 @@
 
 class Tile;
 
-struct Unit
+class Unit
 {
+  public:
+    bool m_canMove;
     int m_type;
     int m_owner;
     int m_power;
     Tile* m_pLoc;
     Tile* m_pTarget;
     Cost m_cost;
+
+    Unit()
+    {
+        m_canMove = true;
+    }
+
+    bool canMove()
+    {
+        return m_canMove;
+    }
+    void move(Tile* target)
+    {
+        m_pTarget = target;
+        m_canMove = false;
+    }
+
+    void resolve()
+    {
+        (*m_pLoc).removeUnit(this);
+        m_pLoc = m_pTarget;
+        (*m_pTarget).addUnit(this);
+    }
 };
 
 #endif
